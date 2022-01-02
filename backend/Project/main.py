96c34e496edd4006f34ecfed4903df8b44b26fed
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-03 01:37:26 +0200

    feat: getdb()
    created method to access local database

--- backend/Project/main.py
+++ backend/Project/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 from .schemas import User
+from .database import engine,SessionLocal
+from sqlalchemy.orm import Session
 
 app=FastAPI()
 
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.post('/user')
 def create_user(request:User):
