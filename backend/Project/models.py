960c90138c07c360fb3ffe8b85f50e3fe3eb6cfa
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-03 01:42:10 +0200

    feat: User model

--- backend/Project/models.py
+++ backend/Project/models.py
@@ -1,1 +1,13 @@
 
+
+
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import Integer, String
+
+
+class User(Base):
+    __tablename__='Users'
+    id=Column(Integer,primary_key=True,index=True)
+    name=Column(String)
+    email=Column(String)
+    password=Column(String)
