528e9be314b2ff1bd6da21fa33d91393ee78deb2
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-03 18:27:41 +0200

    feat: User Repository

--- backend/Project/routers/user.py
+++ backend/Project/routers/user.py
@@ -2,28 +2,19 @@ from fastapi import APIRouter,HTTPException,status,Depends
 from sqlalchemy.orm.session import Session
 from .. import schemas,models,hashing,database
 from sqlalchemy.orm import Session
+from ..repository import user
 
-router=APIRouter()
+router=APIRouter(
+    prefix="/user",
+   tags=['Users']
+)
 
-@router.post('/user',response_model=schemas.ShowUser,tags=['Users'])
+@router.post('/',response_model=schemas.ShowUser)
 def create_user(request:schemas.User,db:Session=Depends(database.get_db)):
-  new_user=models.User(
-      name=request.name,
-      email=request.email,
-      password=hashing.Hash.bcrypt(request.password)
-  )
-  db.add(new_user)
-  db.commit()
-  db.refresh(new_user)
-  return new_user
+    return user.create_user(request,db)
 
 
-@router.get('/user/{id}',response_model=schemas.ShowUser,tags=['Users'])
+
+@router.get('/{id}',response_model=schemas.ShowUser)
 def get_user(id:int,db:Session=Depends(database.get_db)):
-    user=db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with the id {id} was not found"
-        )
-    return user
+    return user.get_user(id,db)
