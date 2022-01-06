d1057114db7ac90d8f1c3f786f35714516c0fecd
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-06 17:52:47 +0200

    fix: Added authentication before using boards routes

--- backend/Project/routers/boards.py
+++ backend/Project/routers/boards.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter,Depends
 from sqlalchemy.orm.session import Session
-from .. import schemas, database
+from .. import schemas, database,oauth2
 from ..repository import boards
 from typing import List
 
@@ -10,13 +10,13 @@ router=APIRouter(
 )
 
 @router.post('/')
-def create_board(request:schemas.Boards,db:Session=Depends(database.get_db)):
+def create_board(request:schemas.Boards,db:Session=Depends(database.get_db),get_current_user:schemas.User=Depends(oauth2.get_current_user)):
     return boards.create_board(request,db)
 
 @router.get('/',response_model=List[schemas.Boards])
-def get_boards(db:Session=Depends(database.get_db)):
+def get_boards(db:Session=Depends(database.get_db),get_current_user:schemas.User=Depends(oauth2.get_current_user)):
     return boards.get_boards(db)
 
 @router.get('/{id}',response_model=schemas.Boards)
-def get_board(id:int,db:Session=Depends(database.get_db)):
+def get_board(id:int,db:Session=Depends(database.get_db),get_current_user:schemas.User=Depends(oauth2.get_current_user)):
     return boards.get_board(id,db)
