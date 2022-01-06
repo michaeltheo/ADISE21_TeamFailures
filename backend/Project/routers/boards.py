c4c914976d3f3da128ed6e2f10a0fd282319ac2f
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-07 01:53:35 +0200

    feat: Added upadate Board Route

--- backend/Project/routers/boards.py
+++ backend/Project/routers/boards.py
@@ -1,15 +1,17 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends,HTTPException,status
 from sqlalchemy.orm.session import Session
 from .. import schemas, database,oauth2
 from ..repository import boards
 from typing import List
+from uuid import UUID
+
 
 router=APIRouter(
     prefix='/board',
     tags=['Board']
 )
 
-@router.post('/')
+@router.post('/',status_code=status.HTTP_201_CREATED)
 def create_board(request:schemas.Boards,db:Session=Depends(database.get_db),get_current_user:schemas.User=Depends(oauth2.get_current_user)):
     return boards.create_board(request,db)
 
@@ -20,3 +22,7 @@ def get_boards(db:Session=Depends(database.get_db),get_current_user:schemas.User
 @router.get('/{id}',response_model=schemas.Boards)
 def get_board(id:int,db:Session=Depends(database.get_db),get_current_user:schemas.User=Depends(oauth2.get_current_user)):
     return boards.get_board(id,db)
+
+@router.put('/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update(id:UUID,request:schemas.Boards,db:Session=Depends(database.get_db),get_current_user:schemas.User=Depends(oauth2.get_current_user)):
+    return boards.update(id,request,db)
