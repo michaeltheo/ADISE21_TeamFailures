e5f2c26adce03ce776724903c0c79c56737f44bc
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-07 01:33:50 +0200

    fix: Updated database URL

--- backend/Project/database.py
+++ backend/Project/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 SQLALCHEMY_DATABASE_URL='postgresql://ctatstkjmjqtex:413949579933eec9393c9eab104e55b9fd62a5c5aa72de5b34e8e65c3eb83858@ec2-54-229-68-88.eu-west-1.compute.amazonaws.com:5432/d5pv25rpn5docu'
-engine=create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread":False})
+engine=create_engine(SQLALCHEMY_DATABASE_URL)
 Base=declarative_base()
 SessionLocal=sessionmaker(bind=engine)
 
