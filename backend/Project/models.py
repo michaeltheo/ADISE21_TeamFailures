eae4526cc561329c5659e4f3550b9e1c359b285a
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-07 01:08:22 +0200

    fix: added players column into Boards Table

--- backend/Project/models.py
+++ backend/Project/models.py
@@ -2,6 +2,8 @@ from sqlalchemy.sql.schema import Column,ForeignKey
 from sqlalchemy.sql.sqltypes import Integer, String
 from sqlalchemy.orm import relationship
 from .database import Base
+from typing import List
+from sqlalchemy.sql.sqltypes import ARRAY
 
 class User(Base):
     __tablename__='Users'
@@ -17,4 +19,5 @@ class Board(Base):
     __tablename__='Board'
     id=Column(Integer,primary_key=True,index=True)
     creator_id=Column(Integer,ForeignKey('Users.id'))
+    players=Column(ARRAY(String,dimensions=2))
     creator=relationship('User',back_populates='board')
