f82dc365709c6e06318b1167724201eec8655b9f
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-07 01:19:47 +0200

    fix: Board Primary changed Type

--- backend/Project/schemas.py
+++ backend/Project/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from sqlalchemy.sql.functions import user
-from typing import Optional
+from typing import Optional,List
+from uuid import UUID
 
 class User(BaseModel):
     name:str
@@ -28,9 +29,10 @@ class TokenData(BaseModel):
     email: Optional[str] = None
 
 class Boards(BaseModel):
-    id:Optional[int]
+    id:Optional[UUID]
     creator_id:int
     creator:Optional[ShowUser]
     players:Optional[ShowUser]
+    board:Optional[List[str]]
     class Config():
         orm_mode=True
