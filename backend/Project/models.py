6f1c618c77557baccadd52707568c90cc7a96964
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-06 17:17:38 +0200

    feat: Added Boards Model

--- backend/Project/models.py
+++ backend/Project/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.schema import Column,ForeignKey
 from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.orm import relationship
 from .database import Base
 
 class User(Base):
@@ -8,3 +9,12 @@ class User(Base):
     name=Column(String)
     email=Column(String)
     password=Column(String)
+    board=relationship('Board',back_populates='creator')
+
+
+# boards Model
+class Board(Base):
+    __tablename__='Board'
+    id=Column(Integer,primary_key=True,index=True)
+    creator_id=Column(Integer,ForeignKey('Users.id'))
+    creator=relationship('User',back_populates='board')
