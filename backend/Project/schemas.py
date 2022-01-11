2d494db6bcb4fe84030fcfa9e49c19222802bff2
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-11 03:02:53 +0200

    fix: updated schema Board

--- backend/Project/schemas.py
+++ backend/Project/schemas.py
@@ -1,24 +1,27 @@
 from pydantic import BaseModel
 from sqlalchemy.sql.functions import user
-from typing import Optional,List
+from typing import Optional, List
 from uuid import UUID
 
+
 class User(BaseModel):
-    name:str
-    email:str
-    password:str
+    name: str
+    email: str
+    password: str
 
 
 class ShowUser(BaseModel):
-    name:str
-    email:str
+    name: str
+    email: str
 
-    class Config():
-        orm_mode=True
+    class Config:
+        orm_mode = True
+
 
 class Login(BaseModel):
-    username:str
-    password:str
+    username: str
+    password: str
+
 
 class Token(BaseModel):
     access_token: str
@@ -28,11 +31,14 @@ class Token(BaseModel):
 class TokenData(BaseModel):
     email: Optional[str] = None
 
+
 class Boards(BaseModel):
-    id:Optional[UUID]
-    creator_id:Optional[int]
-    creator:Optional[ShowUser]
-    players:Optional[List[str]]
-    board:Optional[List]
-    class Config():
-        orm_mode=True
+    id: Optional[UUID]
+    creator_id: Optional[int]
+    creator: Optional[ShowUser]
+    players: Optional[List[str]]
+    board: Optional[List]
+    isFull: Optional[bool]
+
+    class Config:
+        orm_mode = True
