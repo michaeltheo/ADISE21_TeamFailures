488e824e9d1c89bfb686bf1c35c970a2eec72a38
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-11 03:04:48 +0200

    feat: added new Routers

--- backend/Project/routers/boards.py
+++ backend/Project/routers/boards.py
@@ -1,32 +1,47 @@
-from fastapi import APIRouter,Depends,HTTPException,status
+from uuid import UUID
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm.session import Session
-from .. import schemas, database,oauth2
+from sqlalchemy.sql.functions import func, mode
+from sqlalchemy.sql.expression import func
+from .. import schemas, database, models, OAuth2
 from ..repository import boards
 from typing import List
-from uuid import UUID
+
+router = APIRouter(prefix="/boards", tags=["Boards"])
 
 
-router=APIRouter(
-    prefix='/board',
-    tags=['Board']
-)
+@router.post("/")
+def create_board(request: schemas.Boards, db: Session = Depends(database.get_db)):
+    return boards.create_board(request, db)
 
-@router.post('/',status_code=status.HTTP_201_CREATED)
-def create_board(request:schemas.Boards,db:Session=Depends(database.get_db),get_current_user:schemas.User=Depends(oauth2.get_current_user)):
-    return boards.create_board(request,db)
 
-@router.get('/',response_model=List[schemas.Boards])
-def get_boards(db:Session=Depends(database.get_db),get_current_user:schemas.User=Depends(oauth2.get_current_user)):
+@router.get("/random", response_model=schemas.Boards)
+def get_random_board(db: Session = Depends(database.get_db)):
+    return boards.get_random_board(db)
+
+
+@router.get("/", response_model=List[schemas.Boards])
+def get_boards(
+    db: Session = Depends(database.get_db),
+    get_current_user: schemas.User = Depends(OAuth2.get_current_user),
+):
     return boards.get_boards(db)
 
-@router.get('/{id}',response_model=schemas.Boards)
-def get_board(id:UUID,db:Session=Depends(database.get_db),get_current_user:schemas.User=Depends(oauth2.get_current_user)):
-    return boards.get_board(id,db)
 
-@router.put('/{id}',status_code=status.HTTP_202_ACCEPTED)
-def update(id:UUID,request:schemas.Boards,db:Session=Depends(database.get_db),get_current_user:schemas.User=Depends(oauth2.get_current_user)):
-    return boards.update(id,request,db)
+@router.get("/{id}", response_model=schemas.Boards)
+def get_board(
+    id: UUID,
+    db: Session = Depends(database.get_db)
+    # , get_current_user: schemas.User = Depends(OAuth2.get_current_user)
+):
+    return boards.get_board(id, db)
 
-@router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id:UUID,db:Session=Depends(database.get_db),get_current_user:schemas.User=Depends(oauth2.get_current_user)):
-    return boards.destroy(id,db)
+
+@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id: UUID, request: schemas.Boards, db: Session = Depends(database.get_db)):
+    return boards.update(id, request, db)
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: UUID, db: Session = Depends(database.get_db)):
+    return boards.destroy(id, db)
