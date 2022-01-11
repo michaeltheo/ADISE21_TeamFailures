d09866de1e9f44a8dac305a4581fb9d7cd032987
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-11 02:17:08 +0200

    feat:Added origins on Backend

--- backend/Project/main.py
+++ backend/Project/main.py
@@ -2,14 +2,25 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import user,Authentication,boards
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app=FastAPI(
-    
 )
+origins = [
+    '*',
+    "http://localhost:3000",
+]
 
 #when you find a new model added to database
 models.Base.metadata.create_all(engine)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #Routers
 app.include_router(Authentication.router)
