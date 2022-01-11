d1ae8e110cc0ebc8dbe57602376ce55b09459138
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-11 02:14:44 +0200

    feat: Added some Pages

--- backend/Project/routers/Authentication.py
+++ backend/Project/routers/Authentication.py
@@ -1,29 +1,32 @@
-from fastapi import APIRouter,Depends,HTTPException,status
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm.session import Session
 from fastapi.security import OAuth2PasswordRequestForm
-from .. import schemas,database,models,hashing,JWTtoken
+from .. import schemas, database, models, hashing, JWTtoken
 from sqlalchemy.orm import Session
 from ..repository import user
 
-router=APIRouter(
-    prefix='/Auth',
-    tags=['Authentication']
-)
+router = APIRouter(prefix="/Auth", tags=["Authentication"])
 
 
-@router.post('/login')
-def login(request:OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+@router.post("/login")
+def login(
+    request: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(database.get_db),
+):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Invalid Credentials")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials"
+        )
     if not hashing.Hash.verify(user.password, request.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Incorrect password")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Incorrect password"
+        )
 
     access_token = JWTtoken.create_access_token(data={"sub": user.email})
-    return {"access_token": access_token, "token_type": "bearer","user":user}
+    return {"access_token": access_token, "token_type": "bearer", "user": user}
 
-@router.post('/register',response_model=schemas.ShowUser)
-def create_user(request:schemas.User,db:Session=Depends(database.get_db)):
-    return user.create_user(request,db)
+
+@router.post("/register", response_model=schemas.ShowUser)
+def create_user(request: schemas.User, db: Session = Depends(database.get_db)):
+    return user.create_user(request, db)
