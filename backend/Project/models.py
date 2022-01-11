2153b16540e306948587ccfd312cc4314aac883e
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-11 03:02:30 +0200

    fix:Added isFull Column on Boards

--- backend/Project/models.py
+++ backend/Project/models.py
@@ -1,27 +1,29 @@
-from sqlalchemy.sql.schema import Column,ForeignKey
+from sqlalchemy.sql.schema import Column, ForeignKey
 from sqlalchemy.sql.sqltypes import Integer, String
 from sqlalchemy.orm import relationship
 from .database import Base
 from typing import List
-from sqlalchemy.sql.sqltypes import ARRAY
+from sqlalchemy.sql.sqltypes import ARRAY, Boolean
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.dialects import postgresql
 import uuid
 
+
 class User(Base):
-    __tablename__='Users'
-    id=Column(Integer,primary_key=True,index=True)
-    name=Column(String)
-    email=Column(String)
-    password=Column(String)
-    board=relationship('Board',back_populates='creator')
+    __tablename__ = "Users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
+    board = relationship("Board", back_populates="creator")
 
 
 # boards Model
 class Board(Base):
-    __tablename__='Board'
-    id=Column(UUID(as_uuid=True),primary_key=True, default=uuid.uuid4)
-    creator_id=Column(Integer,ForeignKey('Users.id'))
-    players=Column(postgresql.ARRAY(String))
-    board=Column(postgresql.ARRAY(String))
-    creator=relationship('User',back_populates='board')
+    __tablename__ = "Board"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    creator_id = Column(Integer, ForeignKey("Users.id"))
+    players = Column(postgresql.ARRAY(String))
+    board = Column(postgresql.ARRAY(String))
+    isFull = Column(Boolean)
+    creator = relationship("User", back_populates="board")
