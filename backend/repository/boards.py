473de7a0efbf037e5b866f46495dc643986c5463
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-15 01:47:56 +0200

    fix: Update Update_Board_Function

--- backend/repository/boards.py
+++ backend/repository/boards.py
@@ -48,25 +48,6 @@ def get_random_board(db: Session):
     return json_board
 
 
-def update(id: UUID, request: schemas.Boards, db: Session):
-    board = db.query(models.Boards).filter(models.Boards.id == id)
-    board_model = board.first()
-    if not board_model:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Board with id {id} was not found",
-        )
-    if not board_model.isFull:
-        for item in request:
-            if item[1] is not None:
-                print(item[1])
-                board.update({item[0]: item[1]})
-    if len(board_model.players) == 2:
-        board.update({"isFull": True})
-    db.commit()
-    return request
-
-
 def destroy(id: UUID, db: Session):
     board = db.query(models.Boards).filter(models.Boards.id == id)
     if not board.first():
@@ -96,3 +77,69 @@ def get_board(id: UUID, db: Session):
             detail=f"Board with the Uuid {id} is not available",
         )
     return board
+
+# update Functions
+
+
+def update_board_attributes(request, board, board_model):
+    dimension = []
+    if request.board:
+        for index1, array in enumerate(request.board):
+            for index2, item in enumerate(array):
+                if item is not None and len(item) == 1:
+                    dimension = [index1, index2]
+        index = board_model.players.index(request.active_player)
+        if board_model.board[dimension[0]][dimension[1]] is None:
+            request.board[dimension[0]][dimension[1]
+                                        ] = f'{index}{request.board[dimension[0]][dimension[1]]}'
+        else:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                detail="You can play on this board position")
+    for item in request:
+        if item[1] is not None:
+            board.update({item[0]: item[1]})
+
+    return board
+
+
+def get_board_length(board):
+    length = 0
+    for array in board:
+        for item in array:
+            if item != None:
+                length += 1
+    return length
+
+
+def change_active_player(request, board, board_model):
+    index = board_model.players.index(request.active_player)
+    if index == 0:
+        board.update({'active_player': board_model.players[1]})
+    else:
+        board.update({'active_player': board_model.players[0]})
+
+
+def update(id: UUID, request: schemas.Boards, db: Session):
+    board = db.query(models.Boards).filter(models.Boards.id == id)
+    board_model = board.first()
+    if not board_model:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Board with id {id} was not found",
+        )
+    if request.board:
+        if request.active_player == board_model.active_player and (get_board_length(request.board)-get_board_length(board_model.board) == 1):
+            update_board_attributes(request, board, board_model)
+            change_active_player(request, board, board_model)
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_406_NOT_ACCEPTABLE,
+                detail='Its not your turn or you changed more than one board values'
+            )
+    else:
+        update_board_attributes(request, board, board_model)
+    if len(board_model.players) == 2:
+        board.update({"isFull": True})
+    db.commit()
+    json_board = jsonable_encoder(board.first())
+    return JSONResponse(content=json_board)
