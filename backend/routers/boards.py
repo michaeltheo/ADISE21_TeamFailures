4b4fbc2c4672f2fd4683f3c53030265f853ceeb1
Michael Theocharis <michaeltheochar@gmail.com>
2022-01-16 18:21:52 +0200

    fix: Auth

--- backend/routers/boards.py
+++ backend/routers/boards.py
@@ -11,12 +11,12 @@ router = APIRouter(prefix="/boards", tags=["Boards"])
 
 
 @router.post("/")
-def create_board(request: schemas.Boards, db: Session = Depends(database.get_db)):
+def create_board(request: schemas.Boards, db: Session = Depends(database.get_db), get_current_user: schemas.User = Depends(OAuth2.get_current_user)):
     return boards.create_board(request, db)
 
 
 @router.get("/random", response_model=schemas.Boards)
-def get_random_board(db: Session = Depends(database.get_db)):
+def get_random_board(db: Session = Depends(database.get_db), get_current_user: schemas.User = Depends(OAuth2.get_current_user)):
     return boards.get_random_board(db)
 
 
@@ -31,17 +31,16 @@ def get_boards(
 @router.get("/{id}", response_model=schemas.Boards)
 def get_board(
     id: UUID,
-    db: Session = Depends(database.get_db)
-    # , get_current_user: schemas.User = Depends(OAuth2.get_current_user)
+    db: Session = Depends(database.get_db), get_current_user: schemas.User = Depends(OAuth2.get_current_user)
 ):
     return boards.get_board(id, db)
 
 
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
-def update(id: UUID, request: schemas.Boards, db: Session = Depends(database.get_db)):
+def update(id: UUID, request: schemas.Boards, db: Session = Depends(database.get_db), get_current_user: schemas.User = Depends(OAuth2.get_current_user)):
     return boards.update(id, request, db)
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id: UUID, db: Session = Depends(database.get_db)):
+def destroy(id: UUID, db: Session = Depends(database.get_db), get_current_user: schemas.User = Depends(OAuth2.get_current_user)):
     return boards.destroy(id, db)
